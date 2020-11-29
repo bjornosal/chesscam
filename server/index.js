a71313c7adddd3881d45706910321bcc1f2ce6a2
bjornosal <bjorn.olav.salvesen@gmail.com>
2020-11-29 15:36:50 +0100

    Add force https

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ const server = http.createServer(app);
 
 app.use("/", express.static(`${__dirname}/../client/build`));
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS); 
+
 server.listen(port, () => {
   socket(server);
   console.log("Server is listening at :", port);
