5a326f250c97b2d5468b5031b2f82f6e1ec47fc3
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2020-12-29 19:42:20 +0100

    Change custom modal for react-modal

--- client/src/components/CallModal.js
+++ client/src/components/CallModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "react-modal";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideo, faPhone } from "@fortawesome/free-solid-svg-icons";
@@ -8,9 +9,9 @@ const StyledCallModal = styled.div`
   position: fixed;
   width: 600px;
   padding: 50px;
+  text-align: center;
   left: calc(50vw - 350px);
   top: calc(50vh - 60px);
-  text-align: center;
   background-color: var(--main-bg-color);
   display: ${(props) => (props.active ? "block" : "none")};
   z-index: ${(props) => (props.active ? 9999 : "")};
@@ -24,7 +25,7 @@ const StyledIncomingCallButton = styled.button`
   background-color: var(--secondary-color);
   margin: 0.1em;
   border: 2px solid rgba(0, 0, 0, 0.4);
-  border-radius: 24px;
+  border-radius: 12px;
   box-shadow: 0px 1px 2px 2px rgba(0, 0, 0, 0.1);
   transition: 0.5s ease;
   :hover {
@@ -42,7 +43,32 @@ function CallModal({ status, callFrom, startCall, rejectCall }) {
   };
 
   return (
-    <StyledCallModal active={status} className={`call-modal `}>
+    // <StyledCallModal active={status} className={`call-modal `}>
+    <Modal
+      isOpen={status}
+      ariaHideApp={true}
+      style={{
+        overlay: {},
+        content: {
+          border: "1px solid #ccc",
+          background: "#fff",
+          overflow: "auto",
+          WebkitOverflowScrolling: "touch",
+          borderRadius: "4px",
+          outline: "none",
+          padding: "20px",
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          boxShadow: "0px 0px 5px 0px rgba(0, 0, 0, 0.75)",
+          padding: "50px",
+          textAlign: "center",
+        },
+      }}
+    >
       <p>
         <span className="caller" style={{ fontSize: "2em" }}>{`${
           callFrom || "Barnebarnet ditt"
@@ -62,7 +88,7 @@ function CallModal({ status, callFrom, startCall, rejectCall }) {
       >
         <FontAwesomeIcon icon={faPhone} size="2x" />
       </StyledIncomingCallButton>
-    </StyledCallModal>
+    </Modal>
   );
 }
 
